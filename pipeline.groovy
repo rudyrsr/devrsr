804fd49b79ea09f816baeb87b6f8e8b8cc6fd764
rudyrsr <rudy3112rsr@gmail.com>
2024-04-26 02:39:44 -0400

    probando

--- pipeline.groovy
+++ pipeline.groovy
@@ -88,10 +88,8 @@ pipeline{
                     unstash 'backartifact'
                     sh "rm /data/publish/app.jar | true"
                     sh "cp am-core-web-service/target/app.jar /data/publish/"
-                    sh "cd /data/publish/"
-                    sh "docker rmi back-prueba:latest | true"
+                    sh "cd /data/publish/; docker rmi back-prueba:latest | true; cd /data/publish/ ; docker build -t back-prueba:latest ."
                     sh "pwd"
-                    sh "docker build -t back-prueba:latest ."
                     id_imagen= sh(returnStdout: true, script: 'docker images | grep "back-prueba" | awk {print $3}')
                     sh "docker tag $id_imagen 192.168.137.10:8082/v2/repository/docker/back-prueba:latest"
 
