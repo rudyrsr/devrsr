e5eab491b75f09f18de0824b98232aa70d8c3c9a
rudyrsr <rudy3112rsr@gmail.com>
2024-12-26 17:35:26 -0400

    proyecto back

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,5 +1,9 @@
 pipeline{
-    agent any   
+    agent any 
+    tools{
+        maven 'maven-362'
+        jdk 'jdk21'
+    }  
     stages{
         stage("Limpiar"){
             steps{
@@ -10,6 +14,16 @@ pipeline{
             steps{
                 echo "empezando con la creacion de jobs"
             }
+        }
+        stage('descargando proyecto')
+        {
+            steps{
+                git credentialsId: 'Jenkins', branch: "dev", url: "https://github.com/andresmerida/academic-management.git" 
+                sh "mvn -v"
+                sh "pwd"
+                sh "mvn clean compile package"
+               }
+            
         }
     }
 }
