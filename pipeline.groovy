d5017754f238afff8f317517941aa4e5f24554be
rudyrsr <rudy3112rsr@gmail.com>
2024-12-30 18:26:35 -0400

    proyecto back

--- pipeline.groovy
+++ pipeline.groovy
@@ -2,13 +2,6 @@ pipeline{
     agent{
         label 'nod2_slave'
     }
-    tools{
-        maven 'maven-38'
-        jdk 'jdk21'
-    }
-    environment{
-       workspace= "/data/"
-    }
     stages{
         stage("Limpiar"){
             steps{
@@ -19,16 +12,6 @@ pipeline{
             steps{
                 echo "empezando con la creacion de jobs"
             }
-        }
-        stage('descargando proyecto')
-        {
-            steps{
-                git credentialsId: 'git_jenkins', branch: "dev", url: "https://github.com/andresmerida/academic-management.git" 
-                sh "mvn -v"
-                sh "pwd"
-                sh "mvn clean compile package"
-               }
-            
         }
     }
 }
