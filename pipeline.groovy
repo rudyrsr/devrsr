903554bfb61bb8a16965c1f356e974ec610fb595
rudyrsr <rudy3112rsr@gmail.com>
2024-12-30 21:52:59 -0400

    build proyecto spring scan v

--- pipeline.groovy
+++ pipeline.groovy
@@ -28,5 +28,10 @@ pipeline{
                 sh "cp am-core-web-service/target/app.jar /tmp/"
              }
         }
+        stage("Test de vulnerabilidades de seguridad")
+           steps{
+               sh "/grype /tmp/app.jar > informe-scan.txt"
+               rchiveArtifacts artifacts: 'informe-scan.txt', onlyIfSuccessful: true
+           }
     }
 }
