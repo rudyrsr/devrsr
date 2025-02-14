b1434ff9d0176e2811b69b64c85dbcf5e4cfc89a
rudyrsr <rudy3112rsr@gmail.com>
2025-02-13 21:50:14 -0400

    desplegando apps back 2

--- deploy_proyect-back.groovy
+++ deploy_proyect-back.groovy
@@ -22,6 +22,10 @@ pipeline{
                sh "mvn -v"
                sh "pwd"
                sh "mvn clean compile package"
+               sh "mv am-core-web-service/target/am-core-web-service-1.0.0.jar am-core-web-service/target/app.jar"
+               stash includes: 'am-core-web-service/target/app.jar', name: 'backartifact'
+               archiveArtifacts artifacts: 'am-core-web-service/target/app.jar', onlyIfSuccessful: true
+               sh "cp am-core-web-service/target/app.jar /tmp"
            }
         }
     }
