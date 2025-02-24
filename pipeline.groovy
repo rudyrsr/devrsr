08c5f951c1655ff2968ea87a10c13d51618e538c
rudyrsr <rudy3112rsr@gmail.com>
2025-02-24 03:06:29 -0400

    probando

--- pipeline.groovy
+++ pipeline.groovy
@@ -17,8 +17,8 @@ pipeline{
 
     }
 	environment{
-       EMAIL_RECIPIENTS= 'rudy3112rsr@gmail.com'
-       EMAIL_RECIPIENTS_SUPPORT= 'rudy3112rsr@gmail.com'
+       EMAIL_RECIPIENTS= 'rudyrsrcursos@gmail.com'
+       EMAIL_RECIPIENTS_SUPPORT= 'rudyrsrcursos@gmail.com'
     }
     stages{
         stage("Colocar nombre de build")
