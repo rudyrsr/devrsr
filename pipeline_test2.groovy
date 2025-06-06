c9bff3dbf3497943a037afd6a8308cbc857a66ac
rudyrsr <rudy3112rsr@gmail.com>
2025-06-05 20:06:15 -0400

    proyecto test 2

---
+++ pipeline_test2.groovy
@@ -0,0 +1,30 @@
+pipeline{
+    agent {
+       label 'slave1'
+    }
+    tools{
+        jdk 'jdk_2107'
+        maven 'Maven-399'
+    }
+    stages{
+        stage("Limpiar Workspace"){
+            steps{
+                cleanWs()
+            }
+        }
+        stage("Descargar Proyecto")
+        {
+            steps{
+                git credentialsId: 'git_credentials',branch: "dev", url:"https://github.com/andresmerida/academic-management.git"
+            }
+        }
+        stage("Realizar Build")
+        {
+            steps{
+                sh "mvn -v"
+                sh "pwd"
+                sh "mvn clean compile package"
+            }
+        }
+    }
+}
