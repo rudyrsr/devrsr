f1f558dba8b7c9170e395167a0761385233ef676
rudyrsr <rudy3112rsr@gmail.com>
2025-06-05 20:49:18 -0400

    proyecto test 2 cambio3

--- pipeline_test2.groovy
+++ pipeline_test2.groovy
@@ -36,6 +36,9 @@ pipeline{
                 sh "mvn -v"
                 sh "pwd"
                 sh "mvn clean compile package"
+                sh "mvn am-core-web-service/target/am-core-web-service-1.0.0.jar am-core-web-service/target/app.jar"
+                stash includes: 'am-core-web-service/target/app.jar', name: 'backartifact'
+                archiveArtifacts artifacts: 'am-core-web-service/target/app.jar', onlyIfSuccessful: true
             }
         }
     }
