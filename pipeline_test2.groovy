832c1dc8bd440b8fc347b5b84379e27254f79032
rudyrsr <rudy3112rsr@gmail.com>
2025-06-05 20:56:37 -0400

    proyecto test 2 cambio4

--- pipeline_test2.groovy
+++ pipeline_test2.groovy
@@ -36,6 +36,7 @@ pipeline{
                 sh "mvn -v"
                 sh "pwd"
                 sh "mvn clean compile package"
+                sh "pwd"
                 sh "mvn am-core-web-service/target/am-core-web-service-1.0.0.jar am-core-web-service/target/app.jar"
                 stash includes: 'am-core-web-service/target/app.jar', name: 'backartifact'
                 archiveArtifacts artifacts: 'am-core-web-service/target/app.jar', onlyIfSuccessful: true
