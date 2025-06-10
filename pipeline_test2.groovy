4c9b83f3b65b443a77cdd98b245723e4268524c7
rudyrsr <rudy3112rsr@gmail.com>
2025-06-09 22:00:52 -0400

    proyecto test 3 cambio 4

--- pipeline_test2.groovy
+++ pipeline_test2.groovy
@@ -54,7 +54,7 @@ pipeline{
             }
         }
         stage("Test de vulnerabilidades de seguridad"){
-            when(equals expected: 'YES', actual: SCAN_GRYPE)
+            when {equals expected: 'YES', actual: SCAN_GRYPE}
             agent { label 'grype_test'}
             steps{
                script{
@@ -71,7 +71,7 @@ pipeline{
 
         }
         stage("Test con SonarQube"){
-            when(equals expected: 'YES', actual:SCAN_SONARQ)
+            when {equals expected: 'YES', actual:SCAN_SONARQ}
             steps{
                 script{
                     sh "pwd"
