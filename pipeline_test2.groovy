7d6ab12d35254a4bde5c68ae72cd9357eeb40b27
rudyrsr <rudy3112rsr@gmail.com>
2025-06-12 22:30:56 -0400

    proyecto test 4 cambio 1

--- pipeline_test2.groovy
+++ pipeline_test2.groovy
@@ -94,6 +94,20 @@ pipeline{
             }
 
         }
+        stage('Image push images')
+        {
+            agent {label 'node_deploy'}
+            steps{
+                script{
+                    unstash 'backartifact'
+                    sh "rm /data/publish/app.jar | true"
+                    sh "cp am-core-web-service/target/app.jar /data/publish/"
+                    sh "docker rmi  192.168.137.10:8082/docker-test/back-prueba:latest | true; cd /data/publish/ ; docker build -t 192.168.137.10:8082/docker-test/back-prueba:latest ."
+                    sh "docker push  192.168.137.10:8082/docker-test/back-prueba:latest "
+                }
+            }
+        }
+
 
     }
 }
