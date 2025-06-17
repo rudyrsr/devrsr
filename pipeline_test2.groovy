b133add24220b77c44fd95cc5da5ca1946b8e064
rudyrsr <rudy3112rsr@gmail.com>
2025-06-16 20:45:50 -0400

    proyecto back

--- pipeline_test2.groovy
+++ pipeline_test2.groovy
@@ -16,6 +16,10 @@ pipeline{
         choice(name: 'SCAN_GRYPE', choices: ['NO', 'YES'], description: 'Activar si desea escanear con grype')
         choice(name: 'SCAN_SONARQ', choices: ['NO', 'YES'], description: 'Activar si desea escanear con Sonar Qube')
     }
+    environment{
+        EMAIL_RECIPIENTS= 'rudy3112cursos@gmail.com'
+        EMAIL_RECIPIENTS_SUPPORT= 'rudy3112cursos@gmail.com'
+    }
     stages{
         stage("Limpiar Workspace"){
             steps{
@@ -61,6 +65,7 @@ pipeline{
                  unstash 'backartifact'
                  sh "/grype /home/workspace/APP-DEV/job_test2/am-core-web-service/target/app.jar > Informe-scan.txt"
                  archiveArtifacts artifacts: 'Informe-scan.txt', onlyIfSuccessful: true
+                 stash includes: 'Informe-scan.txt', name: 'back_reports'
                  low_vp = sh(returnStdout: true, script: "cat Informe-scan.txt | grep 'Low' | wc -l").trim()
                  medium_vp = sh(returnStdout: true, script: "cat Informe-scan.txt | grep 'Medium' | wc -l").trim()
                  high_vp = sh(returnStdout: true, script: "cat Informe-scan.txt | grep 'High' | wc -l").trim()
@@ -109,5 +114,74 @@ pipeline{
         }
 
 
+    }
+    post {
+        success {
+			unstash  'back_reports'
+		    sendEmail("Successful","${env.EMAIL_RECIPIENTS}","${low_vp}","${medium_vp}","${high_vp}","${critical_vp}")
+	    }
+		failure {
+		    sendEmail("failed", "${env.EMAIL_RECIPIENTS_SUPPORT}","${low_vp}","${medium_vp}","${high_vp}","${critical_vp}")
+		}
+	}
+}
+def sendEmail(status, EMAIL_RECIPIENTS,low_vp,medium_vp,high_vp,critical_vp) {
+    def body1= '''<html>
+  <style>
+     BODY{
+       background-color:white;
+     }
+     TABLE{
+       border-width: 1px;border-style: solid;border-color: blue;border-collapse: collapse;font-family: Verdana;font-size: 10.5pt;
+     }
+     TH{
+      border-width: 1px;padding: 5px;border-style: solid;border-color: blue;background-color:lightblue;
+     }
+     TD{
+      border-width: 1px;padding: 5px;border-style: solid;border-color: blue;text-align:center
+     }
+  </style>
+<body>
+ <a> Hola, </a><br><br>
+ <a> Se muestran los detalles del despliegue del backend</a><br><br>
+  <FONT COLOR="green"> Vulnerabilities found on this version</FONT><br><br>
+  <table>
+   <th>ARTIFACT</th>
+   <th>CRITICO</th>
+   <th>ALTO</th>
+   <th>MEDIO</th>
+   <th>BAJO</th>
+   <tr>
+     <td>BACK-END</td>
+     <td style="color:red;">'''+critical_vp+'''</td>
+     <td style="color:OrangeRed;">'''+high_vp+'''</td>
+     <td style="color:GoldenRod;">'''+medium_vp+'''</td>
+     <td style="color:DarkSeaGreen;">'''+low_vp+'''</td>
+   </tr>
+ </table>
+ <br><br>
+  <FONT COLOR="navy"> Si desea revisar los informes en documentos que se encuentran adjuntos </FONT><br><br>
+ </body>
+</html>'''
+    def body2= '''<html>
+  <style>
+     BODY{
+       background-color:white;
+     }    
+  </style>
+<body>
+ <a> Hi, </a><br><br>
+ <h1 style="color:red;">Actualizacion del BACK-END FALLO</h1><br><br>
+ <br><br>
+ </body>
+</html>'''
+ if(status=="Successful"){
+     
+        emailext body:  body1 + "<br><a>REGARDS</a><br><a>JENKINS</a><br><br>", mimeType: 'text/html',attachmentsPattern:'*.txt', subject: 'Actualizando el BACK-END', to: EMAIL_RECIPIENTS
+     
+        
+    }
+    else{
+        emailext body: body2 + "<br><a>REGARDS</a><br><a>JENKINS</a><br><br>", mimeType: 'text/html', attachLog: true, subject: 'Actualizando el BACK-END', to: EMAIL_RECIPIENTS_SUPPORT
     }
 }
