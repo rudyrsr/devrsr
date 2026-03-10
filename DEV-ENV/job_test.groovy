0602e186a8f18410bd8c180b16a0630d7304e53d
rudyrsr <rudy3112rsr@gmail.com>
2026-03-09 20:32:58 -0400

    iniciando mi proyecto v6

---
+++ DEV-ENV/job_test.groovy
@@ -0,0 +1,28 @@
+pipeline{
+    agent {
+        label 'slave1'
+    }
+    tools{
+        jdk 'java21_slave1'
+        maven 'maven-399'
+    }
+    stages{
+        stage("Limpiar Workspace"){
+            steps{
+                cleanWs()
+            }
+        }
+        stage("Descargar Proyecto"){
+            steps{
+                git credentialsId: 'git_cred', branch: 'dev', url: "https://github.com/andresmerida/academic-management.git"
+            }
+        }
+        stage("Realizar Build"){
+            steps{
+                sh "mvn -v"
+                sh "pwd"
+                sh "mvn clean compile package"
+            }
+        }
+    }
+}
