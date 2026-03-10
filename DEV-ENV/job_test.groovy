beed2542ae38b30e42f3f0e6d739daa68579ce6e
rudyrsr <rudy3112rsr@gmail.com>
2026-03-09 21:46:13 -0400

    iniciando mi proyecto v6.4

--- DEV-ENV/job_test.groovy
+++ DEV-ENV/job_test.groovy
@@ -32,5 +32,14 @@ pipeline{
                 archiveArtifacts artifacts: 'am-core-web-service/target/app.jar', onlyIfSuccessful: true
             }
         }
+        stage("Test de Vulnerabilidades de seguridad"){
+          agent { label 'grype_test'}
+          steps{
+              unstash 'backartifact'
+              sh "/grype /home/workspace/DEV/BACKEND/job_test1/am-core-web-service/target/app.jar > Informe-scan.txt"
+              archiveArtifacts artifacts: 'Informe-scan.txt', onlyIfSuccessful: true
+          }
+
+        }
     }
 }
