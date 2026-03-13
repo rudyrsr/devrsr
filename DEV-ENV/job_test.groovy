b2cfc8514aa27a3d3b82c5e24c3f73f2d291efde
rudyrsr <rudy3112rsr@gmail.com>
2026-03-12 21:23:27 -0400

    proyecto back 93

--- DEV-ENV/job_test.groovy
+++ DEV-ENV/job_test.groovy
@@ -10,6 +10,7 @@ pipeline{
     parameters{
         string defaultValue:'dev',description: 'Colocar el branch a ejecutar', name: 'BRANCH', trim: false
         choice(name: 'SCAN_GRYPE', choices: ['YES','NO'], description:'Seleccione YES si desea escanear las vulnerabilidades de seguridad')
+        choice(name: 'SCAN_SONARQ', choices: ['YES','NO'], description:'Seleccione YES si desea escanear codigo con Sonarqube')
     }
     stages{
         stage("Limpiar Workspace"){
@@ -53,6 +54,27 @@ pipeline{
               archiveArtifacts artifacts: 'Informe-scan.txt', onlyIfSuccessful: true
           }
 
+        }
+        stage("Test con SonarQube"){
+            when {equals expected: 'YES', actual: SCAN_SONARQ}
+            steps{
+                sh "pwd"
+                writeFile encoding: 'UTF-8', file: 'sonar-project.properties', text: """sonar.projectKey=academy
+						sonar.projectName=academy
+						sonar.projectVersion=academy
+						sonar.sourceEncoding=UTF-8
+						sonar.sources=am-core-web-service/src/main/
+						sonar.java.binaries=am-core-web-service/target/
+						sonar.java.libraries=am-core-web-service/target/classes
+						sonar.language=java
+						sonar.scm.provider=git
+						"""
+                        // Sonar Disabled due to we don't have a sonar in tools account yet
+						withSonarQubeEnv('Sonar_CI') {
+						     def scannerHome = tool 'Sonar_CI'
+						     sh "${tool("Sonar_CI")}/bin/sonar-scanner -X"
+						
+            }
         }
     }
 }
