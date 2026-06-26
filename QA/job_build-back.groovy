8ef563ac12a03e09551376330f2c86056000ebc7
rudyrsr <rudy3112rsr@gmail.com>
2026-06-25 20:32:28 -0400

    proyecto back 1

---
+++ QA/job_build-back.groovy
@@ -0,0 +1,26 @@
+pipeline{
+    agent any
+    tools{
+        jdk 'java21_master'
+        maven 'maven-399'
+    }
+    stages{
+        stage("Limpiar Espacio de Trabajo"){
+            steps{
+                 cleanWs()
+            }
+        }
+        stage("Descargar Proyecto"){
+            steps{
+                git credentialsId: 'git_secret', branch: 'dev', url: "https://github.com/andresmerida/academic-management.git"
+            }
+        }
+        stage("Realizar build"){
+            steps{
+                sh "mvn -v"
+                sh "pwd"
+                sh "mvn clean compile package"
+            }
+        }
+    }
+}
