94803e2996da08e32671925dd63dbf0d0aef9517
rudyrsr <rudy3112rsr@gmail.com>
2026-09-16 17:49:53 -0400

    academy front 1

---
+++ front/academy-front.groovy
@@ -0,0 +1,44 @@
+
+def url_repo= "https://git.digitalharborbolivia.com:8081/rsalvatierra.teacher/academy.git" 
+pipeline{
+    agent{
+        //label 'built-in'
+        label 'node_deploy'
+    }
+    stages{
+        stage("limpiar espacio de trabajo"){
+            steps{
+                cleanWs()
+            }
+        }
+        stage("Descargar Proyecto"){
+            steps{
+                git credentialsId: 'gitlab_secret', branch: "devrsr", url:"${url_repo}"
+        
+            }    
+        }
+        stage("Realizar build"){
+            steps{
+                dir('front'){
+                   sh "docker run --rm -v \$(pwd):/usr/src/app -w /usr/src/app node:20.11 sh -c 'npm install && npm run build'"
+                   sh 'tar -cvzf nodemodule.tar.gz node_modules/*'
+                   stash includes: 'nodemodule.tar.gz', name:'nodmodule'
+                   sh 'tar -cvzf dist.tar.gz dist/*'
+                   archiveArtifacts artifacts: 'dist.tar.gz', followSymlinks: false
+                   stash includes: 'dist.tar.gz', name:'distfront'
+                }
+            }
+        }
+        stage("Archivar artefacto"){
+            steps{
+                dir('front'){
+                   sh 'tar -cvzf nodemodule.tar.gz node_modules/*'
+                   stash includes: 'nodemodule.tar.gz', name:'nodmodule'
+                   sh 'tar -cvzf dist.tar.gz dist/*'
+                   archiveArtifacts artifacts: 'dist.tar.gz', followSymlinks: false
+                   stash includes: 'dist.tar.gz', name:'distfront'
+                }
+            }
+        }
+    }
+}
