33364bdafa598bf3bb56662f8baf74c4fd68f05f
rudyrsr <rudy3112rsr@gmail.com>
2026-09-17 17:01:44 -0400

    ejemplo test front 1

--- front/academy-front.groovy
+++ front/academy-front.groovy
@@ -44,7 +44,25 @@ pipeline{
                 }
             }
         }
-       
+        stage("Test de vulnerabilidades con grype"){
+          agent{ label 'agent_grype' }
+          steps{
+               script{
+                unstash 'nodmodule'
+                sh "tar xvfz nodemodule.tar.gz"
+                sh "grype dir:node_modules > Informe-scan-front.txt"
+               stash includes: 'Informe-scan-front.txt', name: 'frontreports'
+               archiveArtifacts artifacts: 'Informe-scan-front.txt', onlyIfSuccessful: true
+               low_back = sh(returnStdout: true, script: "cat Informe-scan-front.txt | grep 'Low' | wc -l").trim()
+               medium_back = sh(returnStdout: true, script: "cat Informe-scan-front.txt | grep 'Medium' | wc -l").trim()
+               high_back = sh(returnStdout: true, script: "cat Informe-scan-front.txt | grep 'High' | wc -l").trim()
+               critical_back = sh(returnStdout: true, script: "cat Informe-scan-front.txt | grep 'Critical' | wc -l").trim()
+               sh "echo 'vulnerabilities: low_back->${low_back}, medium_back->${medium_back}, high_back->${high_back}, critical_back->${critical_back}'"
+               sh "rm -f nodemodule.tar.gz node_modules"
+               }
+          }  
+
+        }
 
     }
 }
