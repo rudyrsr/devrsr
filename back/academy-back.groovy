7f4c5c5739b2069158509f009233090d27212330
rudyrsr <rudy3112rsr@gmail.com>
2026-09-17 17:42:03 -0400

    ejemplo test back 2

--- back/academy-back.groovy
+++ back/academy-back.groovy
@@ -13,6 +13,11 @@ pipeline{
         jdk 'java_21m'
         maven 'maven-399'
     }
+    parameters{
+           string defaultValue: 'devrsr',description: 'Colocar el branch a ejecutar',name: 'BRANCH', trim: 'false'
+           choice (name: 'SCAN_GRYPE', choices: ['YES','NO'],description: 'Seleccione YES si desea escanear vulnerabilidades de seguridad') 
+           choice (name: 'SCAN_SONARQ', choices: ['NO','YES'],description: 'Seleccione YES si desea escanear codigo con Sonarqube')
+    }
     stages{
         stage("limpiar espacio de trabajo"){
             steps{
@@ -44,6 +49,7 @@ pipeline{
             }
         }
         stage("Test de vulnerabilidades con grype"){
+          when {equals expected: 'YES', actual: SCAN_GRYPE}
           agent{ label 'agent_grype' }
           steps{
                script{
@@ -59,6 +65,28 @@ pipeline{
                }
           }  
 
+        }
+        stage("Test con SonarQube"){
+            when {equals expected: 'YES', actual: SCAN_SONARQ}
+            steps{
+                dir ('back'){
+                    script{
+                        sh "pwd"
+                        writeFile encoding: 'UTF-8', file: 'sonar-project.properties', text: """sonar.projectKey=academy-back
+								sonar.projectName=academy.back
+								sonar.projectVersion=1.0.0
+								sonar.sourceEncoding=UTF-8
+								sonar.sources=am-core-web-service/src/main/java
+								sonar.java.binaries=am-core-web-service/target/classes
+								sonar.language=java
+								sonar.scm.provider=git
+                                """
+                        withSonarQubeEnv('Sonar_CI') {
+                             sh "${tool('Sonar_CI')}/bin/sonar-scanner -X"
+                        }
+                    }
+                }
+            }
         }
    }  
 }
--- front/academy-front.groovy
+++ front/academy-front.groovy
@@ -63,27 +63,7 @@ pipeline{
           }  
 
         }
-        stage("Test con SonarQube"){
-            steps{
-                dir ('back'){
-                    script{
-                        sh "pwd"
-                        writeFile encoding: 'UTF-8', file: 'sonar-project.properties', text: """sonar.projectKey=academy-back
-								sonar.projectName=academy.back
-								sonar.projectVersion=1.0.0
-								sonar.sourceEncoding=UTF-8
-								sonar.sources=am-core-web-service/src/main/java
-								sonar.java.binaries=am-core-web-service/target/classes
-								sonar.language=java
-								sonar.scm.provider=git
-                                """
-                        withSonarQubeEnv('Sonar_CI') {
-                             sh "${tool('Sonar_CI')}/bin/sonar-scanner -X"
-                        }
-                    }
-                }
-            }
-        }
+        
 
     }
 }
